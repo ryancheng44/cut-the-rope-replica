2a4b61295f3fcc01517dccade7ede16c0e345b44
ryancheng44 <ryancheng44@gmail.com>
2024-07-02 11:18:01 -0400

    Added Cut the Rope Replica project

---
+++ Cut the Rope Replica/Assets/Scripts/Rope.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public Rigidbody2D hook;
+
+    public GameObject linkPrefab;
+
+    public Weight weight;
+
+    public int links = 7;
+
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    void GenerateRope()
+    {
+        Rigidbody2D previousRb = hook;
+        for (int i = 0; i < links; i++)
+        {
+            GameObject link = Instantiate(linkPrefab, transform);
+            HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
+            joint.connectedBody = previousRb;
+
+            if (i < links - 1)
+            {
+                previousRb = link.GetComponent<Rigidbody2D>();
+            } else
+            {
+                weight.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
+            }
+        }
+    }
+}
---
+++ Cut the Rope Replica/Assets/Scripts/Weight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    public float distanceFromChainEnd = 0.6f;
+
+    public void ConnectRopeEnd(Rigidbody2D endRb)
+    {
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedBody = endRb;
+
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0f, -distanceFromChainEnd);
+    }
+}
